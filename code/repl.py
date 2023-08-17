480a46fec2b1a83cbaec236e9949e4e753f800a3
Thiago MadPin <tpinto@indeed.com>
2023-08-17 03:07:46 +0100

    Working with Lists with different levels

--- code/repl.py
+++ code/repl.py
@@ -13,20 +13,26 @@ class CustomCompleter(Completer):
         super().__init__()
         self.completion_tree = completion_tree
 
-    # @lru_cache(maxsize=128)
+    @lru_cache(maxsize=128)
     def _parse_arguments(self, text):
         return shlex.split(text)
 
     def get_completions(self, document, complete_event):
+        # Convert the input text to lowercase for case-insensitive comparison.
         text = document.text_before_cursor.lower()
+        
+        # Parse the input text to extract arguments.
         arguments = self._parse_arguments(text)
+
         if text[-1] == " ":
             arguments.append("")
 
+        # If there are no arguments or a space was just typed, suggest an empty option.
         if not arguments:
             yield Completion("", start_position=0)
             return
 
+        # Traverse the completion tree based on parsed arguments.
         cur_tree = self.completion_tree
         for arg in arguments[:-1]:
             matching_key = next((k for k in cur_tree if k.lower() == arg), None)
@@ -35,13 +41,28 @@ class CustomCompleter(Completer):
             else:
                 return
 
+        # Get the prefix for suggestions at the current level.
         prefix = arguments[-1]
 
-        if isinstance(cur_tree, (dict, list)):
-            options = [o for o in cur_tree if str(o).lower().startswith(prefix)]
+        # Check if the current level contains options (dict or list).
+        if isinstance(cur_tree, dict):
+            # Special handling for dictionary values that are lists
+            matching_key = next((k for k in cur_tree if k.lower() == prefix), None)
+            if matching_key and isinstance(cur_tree[matching_key], list):
+                for option in cur_tree[matching_key]:
+                    yield Completion(str(option), start_position=0)
+                return
 
+            # Regular handling for other dictionary values
+            options = [o for o in cur_tree if str(o).lower().startswith(prefix)]
             for option in options:
                 yield Completion(str(option), start_position=-len(prefix))
+
+        elif isinstance(cur_tree, list):
+            options = [o for o in cur_tree if str(o).lower().startswith(prefix)]
+            for option in options:
+                yield Completion(str(option), start_position=-len(prefix))
+
 
 
 
@@ -78,6 +99,7 @@ def main():
             "straight": ["North", "South"],
             "curves": ["Left Curve", "Right Curve"],
         },
+        "ai": None
     }
 
     completer = CustomCompleter(options)
